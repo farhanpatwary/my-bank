c86323de131fa3a7f9601c238bf1228ced46454c
farhan <patwary.farhan87@gmail.com>
2020-01-12 21:21:50 +0000

    added interest rate tests

--- src/test/java/com/abc/AccountTest.java
+++ src/test/java/com/abc/AccountTest.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Rule;
 import org.junit.rules.ExpectedException;
 
+import java.util.Date;
+
 public class AccountTest {
 
     @Rule
@@ -78,5 +80,61 @@ public class AccountTest {
     }
 
     // ----------------------------------------------------------
+    // Interest Rate Tests
+
+    @Test
+    public void savingsLessThan1000Interest () {
+        Account savingsAccount = new Account(Account.SAVINGS);
+        savingsAccount.deposit(900);
+        assertEquals(savingsAccount.interestEarned(),0.9,0);
+    }
+
+    @Test
+    public void savings1000Interest() {
+        Account savingsAccount = new Account(Account.SAVINGS);
+        savingsAccount.deposit(1000);
+        assertEquals(savingsAccount.interestEarned(),1,0);
+    }
+
+    @Test
+    public void savingsMoreThan1000Interest() {
+        Account savingsAccount = new Account(Account.SAVINGS);
+        savingsAccount.deposit(1100);
+        assertEquals(savingsAccount.interestEarned(),1.2,0);
+    }
+
+    // MaxiSavings gives 5% interest 
+    // if the last withdrawal is older than 10d
+    @Test
+    public void maxiSavings5Interest() {
+        Account maxiSavingsAccount = new Account(Account.MAXI_SAVINGS);
+        maxiSavingsAccount.deposit(1100);
+        Transaction oldTransaction = new Transaction(-100);
+        // 0 seconds from epoch time
+        // i.e. epoch time -> January 1, 1970, 00:00:00 GMT
+        long time = 0;
+        Date oldDate = new Date(time);
+        oldTransaction.setDate(oldDate);
+        maxiSavingsAccount.addTransaction(oldTransaction);
+        assertEquals(maxiSavingsAccount.interestEarned(),50,0);
+    }
+
+    // Interest rate should be 0.1%
+    @Test
+    public void maxiSavingsSmallInterest() {
+        Account maxiSavingsAccount = new Account(Account.MAXI_SAVINGS);
+        maxiSavingsAccount.deposit(1100);
+        maxiSavingsAccount.withdraw(100);
+        assertEquals(maxiSavingsAccount.interestEarned(),1,0);
+    }
+
+    @Test
+    public void checkingInterest() {
+        Account checkingAccount = new Account(Account.CHECKING);
+        checkingAccount.deposit(1100);
+        checkingAccount.withdraw(100);
+        assertEquals(checkingAccount.interestEarned(),1,0);
+    }
+
 }
 
